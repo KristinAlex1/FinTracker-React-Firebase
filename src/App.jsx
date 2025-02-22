8f6ca1ca2ee0f5dd5e17477da24dc4167ff15fbd
KristinAlex1 <kristinalex2001@gmail.com>
2025-02-21 20:04:35 -0400

    Added Firebase configuration

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import SignUppage from "./pages/SignUppage";
+import SignInpage from "./pages/SignInpage";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/signup" element={<SignUppage/>}/>
+        <Route path="/signin" element={<SignInpage/>}/>
         
       </Routes>
     </Router>
