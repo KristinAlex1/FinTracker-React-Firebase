eb6cf9827cc55c36a6d4212f1caef9d4193215a5
KristinAlex1 <kristinalex2001@gmail.com>
2025-02-21 20:35:07 -0400

    Added sign-up logic

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import SignUppage from "./pages/SignUppage";
 import SignInpage from "./pages/SignInpage";
+import Dashboardpage from "./pages/Dashboardpage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="/signup" element={<SignUppage/>}/>
         <Route path="/signin" element={<SignInpage/>}/>
+        <Route path="/dashboard" element={<Dashboardpage/>}/>
         
       </Routes>
     </Router>
